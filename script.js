ae3cb8a227e16ba6aace9dffc6de05b9627a9edd
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-08-27 12:19:11 +0700

    finish task 3 - add container

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ document.querySelector('body').appendChild(newH3Element);
 
 newH3Element.innerText = "I'm a blue h3!"
 newH3Element.style.color = 'blue';
+
+// Task 3: add <div> with a black border and pink background color
+// with the following elements inside of it:
+//  - another <h1> that says "I'm in a div"
+//  - a <p> that says "ME TOO!"
+//  - Hint for this one: after creating the <div> with createElement,
+// append the <h1> and <p> to it before adding it to the cointainer
+const container = document.createElement('div');
+document.querySelector('body').appendChild(container);
+container.setAttribute('id','container');
+
+const h1 = document.createElement('h1');
+h1.innerText = "I'm in a div";
+container.appendChild(h1);
+
+const p = document.createElement('p');
+p.innerText = "ME TOO!";
+container.appendChild(p);
