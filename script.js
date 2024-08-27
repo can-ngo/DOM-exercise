945097b3cd7c1694f2097c4f654828545516b31f
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-08-27 11:46:08 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Task 1: add <p> with red text that says "Hey I'm red!"
+const newPElement = document.createElement('p');
+document.querySelector('body').appendChild(newPElement);
+
+newPElement.setAttribute('id','first-p');
+newPElement.innerText = "Hey I'm red";
+// document.querySelector('#first-p').innerText = "Hey I'm red!";
+newPElement.style.color = 'red';
+
+// Task 2: add an <h3> with blue text that says "I'm a blue h3!"
+const newH3Element = document.createElement('h3');
