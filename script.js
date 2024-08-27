74202d2f82dfd6a0cb310e6be0a56e05cec1c48f
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-08-27 12:55:39 +0700

    feat: add 3 methods of click event

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // Task 1: add <p> with red text that says "Hey I'm red!"
-const newPElement = document.createElement('p');
-document.querySelector('body').appendChild(newPElement);
+const newPElement = document.createElement('p')
+document.querySelector('body').appendChild(newPElement)
 
-newPElement.setAttribute('id','first-p');
-newPElement.innerText = "Hey I'm red";
+newPElement.setAttribute('id','first-p')
+newPElement.innerText = "Hey I'm red"
 // document.querySelector('#first-p').innerText = "Hey I'm red!";
-newPElement.style.color = 'red';
+newPElement.style.color = 'red'
 
 // Task 2: add an <h3> with blue text that says "I'm a blue h3!"
-const newH3Element = document.createElement('h3');
-document.querySelector('body').appendChild(newH3Element);
+const newH3Element = document.createElement('h3')
+document.querySelector('body').appendChild(newH3Element)
 
 newH3Element.innerText = "I'm a blue h3!"
-newH3Element.style.color = 'blue';
+newH3Element.style.color = 'blue'
 
 // Task 3: add <div> with a black border and pink background color
 // with the following elements inside of it:
@@ -20,14 +20,26 @@ newH3Element.style.color = 'blue';
 //  - a <p> that says "ME TOO!"
 //  - Hint for this one: after creating the <div> with createElement,
 // append the <h1> and <p> to it before adding it to the cointainer
-const container = document.createElement('div');
-document.querySelector('body').appendChild(container);
-container.setAttribute('id','container');
+const container = document.createElement('div')
+document.querySelector('body').appendChild(container)
+container.setAttribute('id','container')
 
-const h1 = document.createElement('h1');
-h1.innerText = "I'm in a div";
-container.appendChild(h1);
+const h1 = document.createElement('h1')
+h1.innerText = "I'm in a div"
+container.appendChild(h1)
 
 const p = document.createElement('p');
-p.innerText = "ME TOO!";
-container.appendChild(p);
+p.innerText = "ME TOO!"
+container.appendChild(p)
+
+// Event
+// Method 2
+const btn2 = document.querySelector('#btn-2')
+btn2.onclick = () => alert("Hello World");
+
+// Method 3
+function alertFunction () {
+    alert("YAY! YOU DID IT!")
+}
+const btn3 = document.querySelector('#btn-3')
+btn3.addEventListener('click', alertFunction);
